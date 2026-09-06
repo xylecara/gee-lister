4a0feefbed43cd758fe0c5c2b967266657e24106
Xyle Cara <xylecara3@gmail.com>
2026-09-06 23:32:51 +0800

    Add arguments, they do nothing for now

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Project initialized!")
+	if len(os.Args) < 2 {
+		fmt.Println("Not enough arguments, try again")
+		return
+	}
+
+	switch os.Args[1] {
+	case "add":
+		fmt.Println("Add worked successfully!")
+	case "update":
+		fmt.Println("Update worked successfully!")
+	case "delete":
+		fmt.Println("Delete worked successfully!")
+	case "mark-in-progress":
+		fmt.Println("Mark-in-progress worked successfully!")
+	case "mark-done":
+		fmt.Println("Mark-done worked successfully!")
+	case "list":
+		fmt.Println("List worked successfully!")
+	default:
+		fmt.Println("Unknown command")
+	}
 }
