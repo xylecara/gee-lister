c4e6f058511404ee084986ac98a5768618ffa601
Xyle Cara <xylecara3@gmail.com>
2026-09-07 23:19:22 +0800

    Add a json file and a json handler with simple add and list features to build upon

---
+++ json_handler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type TaskList struct {
+	Tasks []Task `json:"tasks"`
+}
+
+type Task struct {
+	Task       string `json:"task"`
+	IsFinished bool   `json:"isFinished"`
+}
+
+// From json to go struct
+func Read(jsonFilePath string) (TaskList, error) {
+	file, err := os.Open(jsonFilePath)
+	if err != nil {
+		return TaskList{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	taskList := TaskList{}
+	err = decoder.Decode(&taskList)
+	if err != nil {
+		return TaskList{}, err
+	}
+
+	return taskList, nil
+}
+
+// From go struct to json
+func Write(jsonFilePath string, taskList *TaskList) error {
+	file, err := os.Create(jsonFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	err = encoder.Encode(&taskList)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+	tasksJSON := filepath.Join("json", "tasks.json")
+	tasks, err := Read(tasksJSON)
+	if err != nil {
+		log.Fatal("could not read tasks json, check if it is missing or renamed(must be named tasks.json)")
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("Not enough arguments, try again")
 		return
@@ -13,7 +21,17 @@ func main() {
 
 	switch os.Args[1] {
 	case "add":
-		fmt.Println("Add worked successfully!")
+		if len(os.Args) > 3 {
+			tasks.Tasks = append(tasks.Tasks, Task{os.Args[2], false})
+
+			err := Write(tasksJSON, &tasks)
+			if err != nil {
+				log.Fatal()
+			}
+		} else {
+			fmt.Println("Not enough arguments, try again")
+		}
+
 	case "update":
 		fmt.Println("Update worked successfully!")
 	case "delete":
@@ -23,7 +41,10 @@ func main() {
 	case "mark-done":
 		fmt.Println("Mark-done worked successfully!")
 	case "list":
-		fmt.Println("List worked successfully!")
+		for _, value := range tasks.Tasks {
+			fmt.Println(value)
+		}
+
 	default:
 		fmt.Println("Unknown command")
 	}
